113d64548d6f95543f1fff0697e098649c4e0bb2
lucasvrod <lucas.vrod@hotmail.com>
2020-11-20 09:25:52 -0300

    Initial commit

---
+++ sass-website/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './sass/main.scss'
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
