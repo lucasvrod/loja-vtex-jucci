d0378d5610888388efcd82faebac7ac6d66e9c69
lucasvrod <lucas.vrod@hotmail.com>
2020-11-23 09:43:22 -0300

    Added logobar session

--- sass-website/src/App.js
+++ sass-website/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './sass/main.scss'
 import Header from "./components/Header"
 import Navbar from "./components/Navbar"
+import Logobar from "./components/LogoBar"
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />
+      <Logobar />
     </div>
   );
 }
---
+++ sass-website/src/components/LogoBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const LogoBar = () => {
+  return (
+    <div className="logobar">
+      <div className="container">
+        <div className="logobar__content">
+          <ul className="logobar__left">
+            <li>
+              <a href="">Nossas principais lojas VTEX</a>
+            </li>
+          </ul>
+        </div>
+        <div className="logobar__right">
+          <img src="" alt="" />
+          <img src="" alt="Brastemp" />
+          <img src="" alt="CompraCerta" />
+          <img src="" alt="Consul" />
+          <img src="" alt="TheBar" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LogoBar
