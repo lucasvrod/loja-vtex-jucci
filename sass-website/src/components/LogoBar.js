2e9dcf7b60e8f240cac43a5a1840f772b1ce3c72
lucasvrod <lucas.vrod@hotmail.com>
2020-11-24 17:24:39 -0300

    Updated LogoBar

--- sass-website/src/components/LogoBar.js
+++ sass-website/src/components/LogoBar.js
@@ -5,12 +5,10 @@ const LogoBar = () => {
     <div className="logobar">
       <div className="container">
         <div className="logobar__content">
-          <div className="logobar__left">
-        <a href="">Nossas principais lojas VTEX</a>
-          </div>
         </div>
         <div className="logobar__right">
-          <img src="" alt="" />
+          <label>Nossas principais lojas VTEX</label>
+          <label>→</label>
           <img src="/images/logo-bar/logo-brastemp@3x.svg" alt="Brastemp" />
           <img src="/images/logo-bar/logo-consul@3x.svg" alt="CompraCerta" />
           <img src="/images/logo-bar/logo-consul2@3x.svg" alt="Consul" />
