b6cbec120e85c8b04e99bf9e1c15060668faa360
lucasvrod <lucas.vrod@hotmail.com>
2020-11-24 15:22:40 -0300

    Updated navbar

--- sass-website/src/components/Navbar.js
+++ sass-website/src/components/Navbar.js
@@ -3,22 +3,12 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="container">
-        <div className="navbar__content">
-          <div className="navbar__left">
-            <img src="/images/nav-bar/logoJussiVectorGreen.svg" alt="logo"/>
-          </div>
-          <ul className="navbar__right">
-            <li>
+      <img src="/images/nav-bar/logoJussiVectorGreen.svg" alt="logo" />
+      <div className="navbar__links">
               <a href="#">Nossas Soluções</a>
-            </li>
-            <li>
               <a href="#">Conheça a Jüssi</a>
-            </li>
-          </ul>
+          </div>
         </div>
-      </div>
-    </div>
   )
 }
 
